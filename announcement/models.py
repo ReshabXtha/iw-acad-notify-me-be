87b3008c897e05f96ca176ec9439872c336a2c87
Reshab <reshab.xtha@gmail.com>
2020-08-20 22:14:35 +0545

    fix[announcement model relationship fixed]

--- announcement/models.py
+++ announcement/models.py
@@ -4,15 +4,15 @@ from django.db import models
 from django.utils import timezone
 
 
-class Announcement_File(models.Model):
-    File = models.FileField(blank=True, upload_to='announcement/Media')
-
-
 class Announcement(models.Model):
     Announce_msg = models.TextField()
     Is_pinned = models.BooleanField(default=False)
-    File_id = models.ForeignKey(Announcement_File, on_delete=models.CASCADE, blank=True, null=True)
     DateCreated = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return str(self.Announce_msg)
+
+
+class Announcement_File(models.Model):
+    Announcement = models.ForeignKey(Announcement, on_delete=models.CASCADE, blank=True, null=True)
+    File = models.FileField(blank=True, upload_to='announcement/Media')
--- announcement/serializers.py
+++ announcement/serializers.py
@@ -6,7 +6,7 @@ from announcement.models import Announcement
 class AnnouncementModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Announcement
-        fields = ['Announce_msg', 'Is_pinned', 'File_id', 'DateCreated']
+        fields = ['Announce_msg', 'Is_pinned', 'DateCreated']
 
     def create(self, validated_data):
         return Announcement.objects.create(**validated_data)
@@ -14,6 +14,5 @@ class AnnouncementModelSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         instance.Announce_msg = validated_data['Announce_msg']
         instance.Is_pinned = validated_data['Is_pinned']
-        instance.File_id = validated_data['File_id']
         instance.save()
         return instance
